27dc71232298988b5d198e2f9669d6168c61bd7b
TimourKhan <66032517+TimourKhan@users.noreply.github.com>
2022-03-14 01:01:41 -0400

    Messed with lighting

---
+++ MosqueProject/Assets/DayNight/DayNight.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+    public float theta = 0.1f;
+
+    void Update()
+    {
+        this.transform.RotateAround(Vector3.zero, Vector3.right, theta);
+    }
+}
