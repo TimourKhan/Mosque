5eb3737b73a0db49840dbc4fe9d37e46a0ad2a0e
TimourKhan <66032517+TimourKhan@users.noreply.github.com>
2022-04-11 00:35:27 -0400

    Worked on pause menu

--- MosqueProject/Assets/Scripts/Pause.cs
+++ MosqueProject/Assets/Scripts/Pause.cs
@@ -1,22 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void OnEnable()
-    {
-        LeanTween.move(GetComponent<RectTransform>(), new Vector3(0f, 134f, 0f), .3f).setDelay(.3f).setEase(LeanTweenType.easeOutCubic).setIgnoreTimeScale(true);
-        Time.timeScale = 0f;
+
+    public static bool GameIsPaused;
+    public GameObject pauseMenuUI;
+    public GameObject main;
+    public GameObject exit;
+    [SerializeField] GameObject dimmer;
 
 
-
-    }
 
     // Update is called once per frame
     void Update()
     {
+        /*if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }else
+            {
+                PauseGame();
+            }    
+            }*/
+        }
+
+    public void ResumeGameInitialize()
+    {
+        LeanTween.move(pauseMenuUI.GetComponent<RectTransform>(), new Vector3(0f, 715f, 0f), .3f).setDelay(.1f).setEase(LeanTweenType.easeOutCubic).setIgnoreTimeScale(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Invoke("ResumeGame", .5f);
+    }
+        public void ResumeGame()
+    {
         
+        dimmer.SetActive(false);
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        dimmer.SetActive(true);
+        pauseMenuUI.SetActive(true);
+        LeanTween.move(pauseMenuUI.GetComponent<RectTransform>(), new Vector3(0f, -598f, 0f), .3f).setDelay(.3f).setEase(LeanTweenType.easeOutCubic).setIgnoreTimeScale(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void ExitToMenuDialog()
+    {
+        main.SetActive(false);
+        exit.SetActive(true);
+    }
+    public void TitleScreen()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+
+    public void ReturnToPause()
+    {
+        exit.SetActive(false);
+        main.SetActive(true);
     }
 }
